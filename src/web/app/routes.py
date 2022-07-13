7d5423092e844db17fb9e21f806e8ef2715a7c29
Andreas Langholz <langholz.a@gmail.com>
2022-07-13 02:50:45 +0100

    server backup 2

---
+++ src/web/app/routes.py
@@ -0,0 +1,9 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    user = {
+        'name':'Chandra'
+    }
+    return render_template('index.html', user=user)
